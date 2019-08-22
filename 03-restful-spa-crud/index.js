daa636a8af46f85ae61385f91ea5c47e28f3d1ed
JunilHwang <tjsdlf4261@naver.com>
2019-08-22 16:35:42 +0900

    Complete - restful + spa

--- 03-restful-spa-crud/index.js
+++ 03-restful-spa-crud/index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 
+const list = []
+
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: false }))
+app.get('/list', (req, res) => {
+  res.send({
+    success: true,
+    data: list
+  })
+})
+app.post('/list', (req, res) => {
+  list.push(req.body.content)
+  res.send({ success: true })
+})
+app.put('/list/:idx', (req, res) => {
+  list[req.params.idx] = req.body.content
+  res.send({ success: true })
+})
+app.delete('/list/:idx', (req, res) => {
+  list.splice(req.params.idx, 1)
+  res.send({ success: true })
+})
 app.use('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 })
