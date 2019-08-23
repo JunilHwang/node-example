836a2418d5c99d563ffe833b6cbbb255e6f01a93
JunilHwang <tjsdlf4261@naver.com>
2019-08-23 23:26:12 +0900

    Complete - Chatting with socket.io

--- 05-chat/index.js
+++ 05-chat/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
-const io = require('socket.io')()
+const io = require('socket.io')(http)
 
 io.on('connection', socket => {
   socket.on('msg', v => {
-    console.log(V)
+    io.emit('msgAdd', v)
   })
 })
 
